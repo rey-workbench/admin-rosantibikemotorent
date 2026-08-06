479edfd06d128592f6512df389ba991118c5c3a2
reysilvaa <reynaldsilva123@gmail.com>
2026-08-07 01:42:35 +0700

    refactor: remove unused tanstack query and tiptap extension dependencies

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-export * as Api from './api';
-export * as Types from './types';
-export * as Constants from './constants';
-export * as Utils from './utils';
-export * as Stores from './stores';
-export * as Services from './services';
--- src/lib/stores/index.ts
+++
@@ -1,5 +0,0 @@
-export { toast, type ToastMessage, type ToastType } from './toast';
-export { authStore, isAuthenticated, type AuthState } from './auth';
-export { confirm, type ConfirmOptions, type ConfirmState } from './confirm';
-
-export * from './websocket';
